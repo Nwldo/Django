f18c43ff79cdb51603f94d6104fe59ee9a59f822
Nwldo <regenildoliveira@yahoo.com.br>
2024-02-21 10:00:45 -0300

    Criando o diretório Django para os projetos

---
+++ project/blog/views.py
@@ -0,0 +1,12 @@
+#from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def blog(request):
+    print('blog')
+    return HttpResponse('blog do app 1')
+
+
+def exemplo(request):
+    print('exemplo')
+    return HttpResponse('exemplo do app 1')
