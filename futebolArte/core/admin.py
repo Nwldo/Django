866181c07eaa4b14100435f3f829868a27660fda
Nwldo <regenildoliveira@yahoo.com.br>
2024-02-22 10:54:19 -0300

    falha

--- futebolArte/core/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Clube, Jogador, Competicao, Titulo
-
-# Register your models here.
-admin.site.register(Clube)
-admin.site.register(Jogador)
-admin.site.register(Competicao)
-admin.site.register(Titulo)
