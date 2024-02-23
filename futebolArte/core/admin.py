070629956ef26d303c7930aa6e79b21c38af18a1
Nwldo <regenildoliveira@yahoo.com.br>
2024-02-23 16:11:01 -0300

    personalização Ajuste Idioma,Título da PáĀina Principal, Mude a URL padrão do admin, Crie Grupos de Usuários

--- futebolArte/core/admin.py
+++ futebolArte/core/admin.py
@@ -3,9 +3,18 @@ from .models import Clube, Jogador, Competicao, Titulo, Pais, Estado, Cidade
 from django.utils.html import format_html
 
 # Register your models here.
+
+class JagadorInline(admin.TabularInline):
+    model = Jogador
+    extra = 1
+
+class TituloInline(admin.TabularInline):
+    model = Titulo
+    extra = 1
+
 @admin.register(Clube)
 class ClubeAdmin(admin.ModelAdmin):
-    list_display = ('id', 'escudo','nome', 'ano_fundacao', 'divisao_atual', 'genero', 'pais', 'estado','cidade' )
+    list_display = ('id', 'escudo','nome', 'ano_fundacao', 'divisao_atual', 'genero', 'pais', 'estado','cidade', 'delete' )
 
     def escudo(self, obj):
         width, height = 50, 50
@@ -13,19 +22,29 @@ class ClubeAdmin(admin.ModelAdmin):
         return format_html(
             html.format(url=obj.cover.url, width=width, height=height)
         )
+    inlines = (JagadorInline, TituloInline,)
 
-@admin.register(Jogador)
-class JogadorAdmin(admin.ModelAdmin):
-    list_display = ('id', 'nome', 'foto', 'posicao_principal', 'numero_camisa', 'sexo', 'pais', 'estado', 'cidade', 'clube')
-    list_filter = ['clube']
+
+
+#@admin.register(Jogador)
+#class JogadorAdmin(admin.ModelAdmin):
+    #list_display = ('id', 'nome', 'foto', 'posicao_principal', 'numero_camisa', 'sexo', 'pais', 'estado', 'cidade', 'clube')
+    #list_filter = ['clube']
+
+
 
 @admin.register(Competicao)
 class CompeticaoAdmin(admin.ModelAdmin):
     list_display = ('id', 'nome', 'tipo_disputa', 'categoria')
 
-@admin.register(Titulo)
-class TituloAdmin(admin.ModelAdmin):
-    list_display = ('id', 'resultado', 'ano_conquista', 'data_exata', 'clube', 'competicao')
+
+
+#@admin.register(Titulo)
+#class TituloAdmin(admin.ModelAdmin):
+    #list_display = ('id', 'resultado', 'ano_conquista', 'data_exata', 'clube', 'competicao')
+
+
+
 
 @admin.register(Pais)
 class PaisAdmin(admin.ModelAdmin):
