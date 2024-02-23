0ae301d259add252d1e538843eb4895030c63a8a
Nwldo <regenildoliveira@yahoo.com.br>
2024-02-23 14:13:53 -0300

    Configurando o template no orm django

--- futebolArte/core/admin.py
+++ futebolArte/core/admin.py
@@ -3,12 +3,39 @@ from .models import Clube, Jogador, Competicao, Titulo, Pais, Estado, Cidade
 from django.utils.html import format_html
 
 # Register your models here.
-admin.site.register(Clube)
-admin.site.register(Jogador)
-admin.site.register(Competicao)
-admin.site.register(Titulo)
-admin.site.register(Pais)
-admin.site.register(Estado)
-admin.site.register(Cidade)
+@admin.register(Clube)
+class ClubeAdmin(admin.ModelAdmin):
+    list_display = ('id', 'escudo','nome', 'ano_fundacao', 'divisao_atual', 'genero', 'pais', 'estado','cidade' )
 
-list_display = ['image_tag']
+    def escudo(self, obj):
+        width, height = 50, 50
+        html = '<img src="/{url}" width="{width}" height={height} />'
+        return format_html(
+            html.format(url=obj.cover.url, width=width, height=height)
+        )
+
+@admin.register(Jogador)
+class JogadorAdmin(admin.ModelAdmin):
+    list_display = ('id', 'nome', 'foto', 'posicao_principal', 'numero_camisa', 'sexo', 'pais', 'estado', 'cidade', 'clube')
+    list_filter = ['clube']
+
+@admin.register(Competicao)
+class CompeticaoAdmin(admin.ModelAdmin):
+    list_display = ('id', 'nome', 'tipo_disputa', 'categoria')
+
+@admin.register(Titulo)
+class TituloAdmin(admin.ModelAdmin):
+    list_display = ('id', 'resultado', 'ano_conquista', 'data_exata', 'clube', 'competicao')
+
+@admin.register(Pais)
+class PaisAdmin(admin.ModelAdmin):
+    list_display = ('id', 'nome', 'continente')
+
+@admin.register(Estado)
+class EstadoAdmin(admin.ModelAdmin):
+    list_display = ('id','nome', 'regiao')
+
+@admin.register(Cidade)
+class CidadeAdmin(admin.ModelAdmin):
+    list_display = ('id', 'nome', 'populacao')
+
