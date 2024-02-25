f0e924cf3d5d20b75e52955eb3c9889b3e8eaa6d
Nwldo <regenildoliveira@yahoo.com.br>
2024-02-25 14:44:18 -0300

    manutenção do projeto futebolArte e criação do projeto_agenda

--- projeto_agenda/projeto_agenda/urls.py
+++ projeto_agenda/projeto_agenda/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for futebolArte project.
+URL configuration for projeto_agenda project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.0/topics/http/urls/
@@ -15,14 +15,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
-
+from django.urls import path
 
 urlpatterns = [
-    path('adminfutebolarte/', admin.site.urls),
-    path('', include('core.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
-#urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path('admin/', admin.site.urls),
+]
