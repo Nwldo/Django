25b3570a1f18230fd034fa377a086332efd47218
Nwldo <regenildoliveira@yahoo.com.br>
2024-03-23 19:16:43 -0300

    Configurando views - método GET

--- django API/api_rest/views.py
+++ django API/api_rest/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
 
-# Create your views here.
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+
+from .models import User
+from .serializers import UserSerializer
+
+import json
+
+from . import funcoes as fn
+
+@api_view(['GET'])
+def get_users(request):
+
+    if request.method == 'GET':
+
+        users = User.objects.all()                          # Get all objects in User's database (It returns a queryset)
+
+        serializer = UserSerializer(users, many=True)       # Serialize the object data into json (Has a 'many' parameter cause it's a queryset)
+
+        return Response(serializer.data)                    # Return the serialized data
+    
+    return Response(status=status.HTTP_400_BAD_REQUEST)
