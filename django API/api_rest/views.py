2dc5944e600239ca82884e7530e0adcdda5e6478
Nwldo <regenildoliveira@yahoo.com.br>
2024-03-24 06:46:59 -0300

    Realizando teste de GET, POST, PUT, DELETE

--- django API/api_rest/views.py
+++ django API/api_rest/views.py
@@ -1,26 +1,41 @@
-from django.shortcuts import render
-from django.http import HttpResponse, JsonResponse
-
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
 from rest_framework import status
-
+from rest_framework.response import Response
+from rest_framework.views import APIView
 from .models import User
 from .serializers import UserSerializer
 
-import json
+class UserView(APIView):
 
-from . import funcoes as fn
-
-@api_view(['GET'])
-def get_users(request):
-
-    if request.method == 'GET':
-
-        users = User.objects.all()                          # Get all objects in User's database (It returns a queryset)
-
-        serializer = UserSerializer(users, many=True)       # Serialize the object data into json (Has a 'many' parameter cause it's a queryset)
-
-        return Response(serializer.data)                    # Return the serialized data
+    def get(self, request):
+        usuarios = User.objects.all()
+        serializer = UserSerializer(usuarios, many=True)
+        return Response(serializer.data, status=200)
     
-    return Response(status=status.HTTP_400_BAD_REQUEST)
+    def post(self, request):
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=201)
+        else:
+            return Response(serializer.errors, status=400)
+        
+
+    def put(self, request, *args, **kwargs):
+        """
+        Atualiza um usuário existente com base no ID (pk).
+        """
+        pk = kwargs.get('pk')  # Obtém o ID do usuário da URL
+        try:
+            user = User.objects.get(pk=pk)
+        except User.DoesNotExist:
+            return Response({"error": "Usuário não encontrado."}, status=404)
+
+        serializer = UserSerializer(user, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=200)
+        else:
+            return Response(serializer.errors, status=400)
+
+
+    
