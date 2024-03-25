f50758daa95dba0416c8d39cb12616a33e2e4088
Nwldo <regenildoliveira@yahoo.com.br>
2024-03-25 11:21:00 -0300

    aplicação com api

---
+++ apirest/api_root/api_root/urls.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from django.urls import path
+from api_rest.views import UserListAPIView, UserUpdateAPIView
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('users/', UserListAPIView.as_view(), name='user-list-create'),
+    path('users/<int:pk>/', UserUpdateAPIView.as_view(), name='user-update-destroy'),
+]
