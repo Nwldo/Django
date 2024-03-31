c01a932af0c1bf6c751f056c6b825aefd3536cc1
Nwldo <regenildoliveira@yahoo.com.br>
2024-03-30 21:30:46 -0300

    Modificacão no settings

--- eryckgym/eryckgym/eryckgym/settings.py
+++ eryckgym/eryckgym/eryckgym/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool)
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS').split(',')
+ALLOWED_HOSTS = ['eryckgym.onrender.com']
 
 CSRF_TRUSTED_ORIGINS = ['https://eryckgym.onrender.com']
 # Application definition
