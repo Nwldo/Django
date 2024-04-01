6a4cb081f006100b4d4db55fe7ae8316d275fc78
Nwldo <regenildoliveira@yahoo.com.br>
2024-04-01 11:47:46 -0300

    Alteração no cadastro/models/aluno

--- eryckgym/eryckgym/cadastro/models.py
+++ eryckgym/eryckgym/cadastro/models.py
@@ -2,12 +2,22 @@ from django.db import models
 from treinos.models import Treino
 
 class Aluno(models.Model):
+    SEXO_CHOICES = (
+        ("F", "Feminino"),
+        ("M", "Masculino"),
+        ("N", "Nenhuma das opções")
+    )
+
+
     nome = models.CharField(max_length=100)
+    sexo = models.CharField(max_length=1, choices=SEXO_CHOICES, default='N')
     idade = models.IntegerField(default=0)
+    cpf = models.IntegerField(default=0)
+    telefone = models.IntegerField(default=0)
     treino = models.OneToOneField(Treino, null=True, blank=True, on_delete=models.SET_NULL)
 
     def __str__(self):
-            return f'Nome: {self.nome} | Idade: {self.idade} | Treino {self.treino}'
+            return f'Nome: {self.nome} | Sexo: {self.sexo} | Idade: {self.idade} | CPF: {self.cpf} | Telefone: {self.telefone} | Treino {self.treino}'
 
 class Professor(models.Model):
     nome = models.CharField(max_length=100)
