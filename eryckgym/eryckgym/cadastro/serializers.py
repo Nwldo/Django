f5d55593272180f609874520daaa77f7c4bcc39f
Nwldo <regenildoliveira@yahoo.com.br>
2024-04-02 09:24:52 -0300

    Modificando importe serializers

--- eryckgym/eryckgym/cadastro/serializers.py
+++ eryckgym/eryckgym/cadastro/serializers.py
@@ -1,12 +1,12 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework import serializers
 from cadastro.models import Aluno, Professor
 
-class AlunoSerializer(ModelSerializer):
+class AlunoSerializer(serializers.ModelSerializer):
     class Meta:
         model: Aluno
         fields = ['id', 'nome', 'sexo', 'idade', 'cpf', 'telefone', 'treino']
 
-class ProfessorSerializer(ModelSerializer):
+class ProfessorSerializer(serializers.ModelSerializer):
     class Meta:
         model: Professor
         fields = ['id', 'nome', 'especialidade', 'treinos']
