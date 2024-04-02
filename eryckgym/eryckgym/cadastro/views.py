52e3ebedf117965abb8d9e6cac836f600f4c8969
Nwldo <regenildoliveira@yahoo.com.br>
2024-04-02 08:06:01 -0300

    Teste api cadastro

--- eryckgym/eryckgym/cadastro/views.py
+++ eryckgym/eryckgym/cadastro/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from django.http import JsonResponse
+from .models import Aluno, Professor
 
-# Create your views here.
+def novo_aluno(request):
+    if request.method == 'POST':
+        nome = request.POST.get('nome')
+        aluno = Aluno.objects.create(nome=nome)
+        return JsonResponse({'id': aluno.id, 'nome': aluno.nome})
+
+def listar_alunos(request):
+    alunos = Aluno.objects.all()
+    data = [{'id': aluno.id, 'nome': aluno.nome} for aluno in alunos]
+    return JsonResponse(data, safe=False)
+
+def detalhes_aluno(request, id):
+    aluno = get_object_or_404(Aluno, id=id)
+    data = {'id': aluno.id, 'nome': aluno.nome}
+    return JsonResponse(data)
+
+def remover_aluno(request, id):
+    aluno = get_object_or_404(Aluno, id=id)
+    aluno.delete()
+    return JsonResponse({'message': 'Aluno removido com sucesso'})
+
+def editar_aluno(request, id):
+    aluno = get_object_or_404(Aluno, id=id)
+    if request.method == 'PUT':
+        nome = request.POST.get('nome')
+        aluno.nome = nome
+        aluno.save()
+        return JsonResponse({'message': 'Aluno atualizado com sucesso'})
--- eryckgym/eryckgym/eryckgym/urls.py
+++ eryckgym/eryckgym/eryckgym/urls.py
@@ -2,9 +2,12 @@
 from django.contrib import admin
 from django.urls import path
 from treinos.views import ListaExerciciosView, ExeciciosUpdateAPIView
+from cadastro.views import novo_aluno, listar_alunos, detalhes_aluno, remover_aluno, editar_aluno
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/exercicios', ListaExerciciosView.as_view()),
     path('api/exercicios/<int:pk>/', ExeciciosUpdateAPIView.as_view(),),
+    path('alunos/', listar_alunos),
+    path('alunos/novo/', novo_aluno),
 ]
