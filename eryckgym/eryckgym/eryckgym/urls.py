dec5c39dea3e5abe6e91ec8338d6959ee8923b02
Nwldo <regenildoliveira@yahoo.com.br>
2024-04-02 14:37:49 -0300

    Testando cliente web

--- eryckgym/eryckgym/eryckgym/urls.py
+++ eryckgym/eryckgym/eryckgym/urls.py
@@ -1,15 +1,16 @@
 
 from django.contrib import admin
-from django.urls import path
-from treinos.views import ListaExerciciosView, ExeciciosUpdateAPIView
-from cadastro.views import ListaAlunosView, AlunosUpdateAPIView, ListaProfessorView, ProfessoresUpdateAPIView
+from django.urls import path, include
+#from treinos.views import ListaExerciciosView, ExeciciosUpdateAPIView
+#from cadastro.views import ListaAlunosView, AlunosUpdateAPIView, ListaProfessorView, ProfessoresUpdateAPIView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/exercicios', ListaExerciciosView.as_view()),
-    path('api/exercicios/<int:pk>/', ExeciciosUpdateAPIView.as_view(),),
-    path('api/alunos', ListaAlunosView.as_view()),
-    path('api/alunos/<int:pk>/', AlunosUpdateAPIView.as_view()),
-    path('api/professores', ListaProfessorView.as_view()),
-    path('api/professores/<int:pk>/', ProfessoresUpdateAPIView.as_view()),
+    path('', include('cadastro.urls')),
+    #path('api/exercicios', ListaExerciciosView.as_view()),
+    #path('api/exercicios/<int:pk>/', ExeciciosUpdateAPIView.as_view(),),
+    #path('api/alunos', ListaAlunosView.as_view()),
+    #path('api/alunos/<int:pk>/', AlunosUpdateAPIView.as_view()),
+    #path('api/professores', ListaProfessorView.as_view()),
+    #path('api/professores/<int:pk>/', ProfessoresUpdateAPIView.as_view()),
 ]
