1cc8674b0adacc53b73fe093cca8b731ac9761c4
Nwldo <regenildoliveira@yahoo.com.br>
2024-04-02 10:25:21 -0300

    Verificando erro Class AlunoSerializer

--- eryckgym/eryckgym/cadastro/serializers.py
+++ eryckgym/eryckgym/cadastro/serializers.py
@@ -1,12 +1,14 @@
 from rest_framework.serializers import ModelSerializer
 from cadastro.models import Aluno, Professor
 
+
 class AlunoSerializer(ModelSerializer):
     class Meta:
         model: Aluno
-        fields = ['id', 'nome', 'sexo', 'idade', 'cpf', 'telefone', 'treino']
+        fields = '__all__'
 
 class ProfessorSerializer(ModelSerializer):
     class Meta:
         model: Professor
         fields = ['id', 'nome', 'especialidade', 'treinos']
+
