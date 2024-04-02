3146bd2b6f7202d0a69ca887a8a49fc3ea368689
Nwldo <regenildoliveira@yahoo.com.br>
2024-04-02 11:40:37 -0300

    Alterando as rotas do aluno e professor

--- eryckgym/eryckgym/eryckgym/urls.py
+++ eryckgym/eryckgym/eryckgym/urls.py
@@ -2,14 +2,14 @@
 from django.contrib import admin
 from django.urls import path
 from treinos.views import ListaExerciciosView, ExeciciosUpdateAPIView
-from cadastro.views import ListaAlunosView, AlunosUpdateAPIView
+from cadastro.views import ListaAlunosView, AlunosUpdateAPIView, ListaProfessorView, ProfessoresUpdateAPIView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/exercicios', ListaExerciciosView.as_view()),
     path('api/exercicios/<int:pk>/', ExeciciosUpdateAPIView.as_view(),),
     path('api/alunos', ListaAlunosView.as_view()),
-    path('api/alunos/<int:pk>/', ListaAlunosView.as_view()),
-    path('api/professores', ListaAlunosView.as_view()),
-    path('api/professores/<int:pk>/', ListaAlunosView.as_view()),
+    path('api/alunos/<int:pk>/', AlunosUpdateAPIView.as_view()),
+    path('api/professores', ListaProfessorView.as_view()),
+    path('api/professores/<int:pk>/', ProfessoresUpdateAPIView.as_view()),
 ]
