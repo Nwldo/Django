923135f6cbb50838dc77c52ff73c78e18e1332bb
Nwldo <regenildoliveira@yahoo.com.br>
2024-04-02 08:37:23 -0300

    Teste 1 api cadastro

--- eryckgym/eryckgym/cadastro/views.py
+++ eryckgym/eryckgym/cadastro/views.py
@@ -1,32 +1,48 @@
-from django.shortcuts import get_object_or_404
-from django.http import JsonResponse
-from .models import Aluno, Professor
 
-def novo_aluno(request):
-    if request.method == 'POST':
-        nome = request.POST.get('nome')
-        aluno = Aluno.objects.create(nome=nome)
-        return JsonResponse({'id': aluno.id, 'nome': aluno.nome})
+from django.http import Http404
+from rest_framework import status
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from cadastro.serializers import AlunoSerializer,ProfessorSerializer
+from cadastro.models import Aluno, Professor
 
-def listar_alunos(request):
-    alunos = Aluno.objects.all()
-    data = [{'id': aluno.id, 'nome': aluno.nome} for aluno in alunos]
-    return JsonResponse(data, safe=False)
+# CBV --> Class Based View
+class ListaAlunosView(APIView):
 
-def detalhes_aluno(request, id):
-    aluno = get_object_or_404(Aluno, id=id)
-    data = {'id': aluno.id, 'nome': aluno.nome}
-    return JsonResponse(data)
-
-def remover_aluno(request, id):
-    aluno = get_object_or_404(Aluno, id=id)
-    aluno.delete()
-    return JsonResponse({'message': 'Aluno removido com sucesso'})
-
-def editar_aluno(request, id):
-    aluno = get_object_or_404(Aluno, id=id)
-    if request.method == 'PUT':
-        nome = request.POST.get('nome')
-        aluno.nome = nome
-        aluno.save()
-        return JsonResponse({'message': 'Aluno atualizado com sucesso'})
+    def get(self, request):
+        alunos = Aluno.objects.all()
+        serializer = AlunoSerializer(alunos, many=True)
+        return Response(serializer.data, status=200)
+    
+    def post(self, request):
+        serializer = AlunoSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=201)
+        else:
+            return Response(serializer.errors, status=400)
+        
+class AlunosUpdateAPIView(APIView):
+    def get_object(self, pk):
+            try:
+                return Aluno.objects.get(pk=pk)
+            except Aluno.DoesNotExist:
+                raise Http404
+    
+    def get(self, request, pk):
+        alunos = self.get_object(pk)
+        serializer = AlunoSerializer(alunos)
+        return Response(serializer.data)
+    
+    def put(self, request, pk):
+        alunos = self.get_object(pk)
+        serializer = AlunoSerializer(alunos, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    def delete(self, request, pk):
+        alunos = self.get_object(pk)
+        alunos.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
--- eryckgym/eryckgym/eryckgym/urls.py
+++ eryckgym/eryckgym/eryckgym/urls.py
@@ -2,12 +2,12 @@
 from django.contrib import admin
 from django.urls import path
 from treinos.views import ListaExerciciosView, ExeciciosUpdateAPIView
-from cadastro.views import novo_aluno, listar_alunos, detalhes_aluno, remover_aluno, editar_aluno
+from cadastro.views import ListaAlunosView, AlunosUpdateAPIView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/exercicios', ListaExerciciosView.as_view()),
     path('api/exercicios/<int:pk>/', ExeciciosUpdateAPIView.as_view(),),
-    path('alunos/', listar_alunos),
-    path('alunos/novo/', novo_aluno),
+    path('api/alunos', ListaAlunosView.as_view()),
+    path('api/alunos/<int:pk>/', ListaAlunosView.as_view()),
 ]
